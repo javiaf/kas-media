597823ebd8037b7f8f88e6b2c51cef39a1f9be20
Miguel París Díaz <mparisdiaz@gmail.com>
2011-10-20 12:41:04 +0200

    Improve VideoCodecType management.
    Use generic Collection in methods interface and
    upper case in codec names.

--- src/com/kurento/kas/media/codecs/VideoCodecType.java
+++ src/com/kurento/kas/media/codecs/VideoCodecType.java
@@ -18,6 +18,7 @@
 package com.kurento.kas.media.codecs;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 import com.kurento.kas.media.exception.CodecNotSupportedException;
 
@@ -27,48 +28,48 @@ import com.kurento.kas.media.exception.CodecNotSupportedException;
  * 
  */
 public enum VideoCodecType {
-	H264(0, new String[]{ "H264", "h264" }),
-	MPEG4(1, new String[]{ "MPEG4", "mpeg4", "MP4V-ES", "mp4v-es" }),
-	H263(2, new String[]{ "H263", "h263", "H263-1998", "h263-1998", "H263-2000", "h263-2000" });
+	H264(0, new String[]{ "H264" }),
+	MPEG4(1, new String[]{ "MPEG4", "MP4V-ES" }),
+	H263(2, new String[]{ "H263", "H263-1998", "H263-2000" });
 	
 	private int codecID;
-	private ArrayList<String> arrayCodecStrings;
+	private Collection<String> collectionCodecStrings;
 	
 	//private int[] supportedBitrates
 	//private int[] supportedFramerates
 
 	private VideoCodecType(int codecID, String[] codecStrings) {
 		this.codecID = codecID;
-		arrayCodecStrings = new ArrayList<String>();
+		collectionCodecStrings = new ArrayList<String>();
 		for (String s : codecStrings)
-			arrayCodecStrings.add(s);
+			collectionCodecStrings.add(s);
 	}
 
 	public int getCodecID() {
 		return codecID;
 	}
 
-	public ArrayList<String> getArrayCodecStrings() {
-		return arrayCodecStrings;
+	public Collection<String> getCollectionCodecStrings() {
+		return collectionCodecStrings;
 	}
 	
 	public static int getCodecIdFromName(String codecName) throws CodecNotSupportedException {
-		if (H264.arrayCodecStrings.contains(codecName))
+		if (H264.collectionCodecStrings.contains(codecName.toUpperCase()))
 			return H264.codecID;
-		else if (MPEG4.arrayCodecStrings.contains(codecName))
+		else if (MPEG4.collectionCodecStrings.contains(codecName.toUpperCase()))
 			return MPEG4.codecID;
-		else if (H263.arrayCodecStrings.contains(codecName))
+		else if (H263.collectionCodecStrings.contains(codecName.toUpperCase()))
 			return H263.codecID;
 
 		throw new CodecNotSupportedException("Codec not supported");
 	}
 	
 	public static VideoCodecType getCodecTypeFromName(String codecName) throws CodecNotSupportedException {
-		if (H264.arrayCodecStrings.contains(codecName))
+		if (H264.collectionCodecStrings.contains(codecName.toUpperCase()))
 			return H264;
-		else if (MPEG4.arrayCodecStrings.contains(codecName))
+		else if (MPEG4.collectionCodecStrings.contains(codecName.toUpperCase()))
 			return MPEG4;
-		else if (H263.arrayCodecStrings.contains(codecName))
+		else if (H263.collectionCodecStrings.contains(codecName.toUpperCase()))
 			return H263;
 
 		throw new CodecNotSupportedException("Codec not supported");
